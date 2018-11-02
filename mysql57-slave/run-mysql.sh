eb891115252e3c5a36e78df47e0bfd87bc2c3037
guojun <guojun@moneynigeria.com>
2018-11-02 10:05:55 +0000

    Add conf and run Script.

---
+++ mysql57-slave/run-mysql.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+### 设置绝对路径
+MYSQL57_HOME="/home/mysqlstack/mysql57-slave"
+
+### 初始启动，并设置默认密码
+sudo -i docker run -d \
+  --name mysql57-slave \
+  -p 3001:3306 \
+  -e MYSQL_ROOT_PASSWORD=root \
+  -v ${MYSQL57_HOME}/conf:/etc/mysql \
+  -v ${MYSQL57_HOME}/data:/var/lib/mysql \
+  -v ${MYSQL57_HOME}/logs:/var/log/mysql \
+  mysql:5.7.23
